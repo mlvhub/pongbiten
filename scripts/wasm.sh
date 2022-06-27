468c2045d6792eec1375856fd64c49182327c8fb
Miguel Lopez V <miguellova@gmail.com>
2022-06-27 23:54:06 +0100

    Make sure script works without any build folder

--- scripts/wasm.sh
+++ scripts/wasm.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-rm -rf ./build/*/**
+rm -rf ./build
+mkdir build
 GOOS=js GOARCH=wasm go build -o ./build/pong.wasm pong
 cp $(go env GOROOT)/misc/wasm/wasm_exec.js ./build/
 cp index.html ./build
